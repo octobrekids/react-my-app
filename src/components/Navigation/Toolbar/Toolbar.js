a4dbfa2dd7341a5b9b973dd2560e2f54d8af2cc1
octobrekids <chnrd.k@gmail.com>
2020-07-07 02:47:49 +0700

    responsive logo

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,15 +1,16 @@
-import React from 'react'
-import classes from './Toolbar.css'
-import Logo from '../../Logo/Logo'
-import NavigationItems from "../NavigationItems/NavigationItems"
+import React from "react";
+import classes from "./Toolbar.css";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
 const toolbar = (props) => {
-    return(
+  return (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
-        <Logo/>
-        <NavigationItems />
-
+      <div>MENU</div>
+      <div className={classes.Logo}>
+        <Logo />
+      </div>
+      <NavigationItems />
     </header>
-    )
-}
+  );
+};
 export default toolbar;
